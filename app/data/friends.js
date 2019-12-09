8e5dac1f3d4c88ccda8a40c03f7415152d0e49c4
Harris <hglane@crimson.ua.edu>
2019-12-08 19:31:53 -0600

    My friends are all here now!

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Travis Morris",
+        photo: "https://ca.slack-edge.com/TMB40JTPG-UM63DEJCS-684a10404862-512",
+        scores: [5, 1, 3, 3, 5, 1, 2, 5, 5, 4]
+    },
+    {
+        name: "Christopher Sinsalath",
+        photo: "https://ca.slack-edge.com/TMB40JTPG-ULYS9CMSP-4a4fe225f6eb-512",
+        scores: [5, 3, 2, 2, 5, 1, 1, 4, 1, 4]
+    },
+    {
+        name: "Brad Eichenberg",
+        photo: "https://ca.slack-edge.com/TMB40JTPG-UMKA90RJB-3e339f926fd6-512",
+        scores: [3, 2, 2, 2, 4, 5, 1, 2, 4, 3]
+    },
+    {
+        name: "Harris Lane",
+        photo: "https://ca.slack-edge.com/TMB40JTPG-UMJ2TDV7G-261a4e4ce979-512",
+        scores: [1, 5, 5, 4, 4, 5, 1, 3, 4, 5]
+    },
+    {
+        name: "Byron Ferguson",
+        photo: "https://ca.slack-edge.com/TMB40JTPG-UM9NLK0A1-1b85fd237c6e-512",
+        scores: [4, 4, 4, 4, 1, 4, 5, 3, 4, 1]
+    },
+    {
+        name: "Robert Davdison Jr",
+        photo: "https://ca.slack-edge.com/TMB40JTPG-ULYMQ0RDZ-deb494e65b57-512",
+        scores: [2, 3, 3, 3, 5, 1, 2, 4, 5, 2]
+    },
+];
+
+module.exports = friends;
