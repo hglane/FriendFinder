35c27a09a12cceee83485b4060af7ebe5c965ddc
Harris <hglane@crimson.ua.edu>
2019-12-08 22:44:11 -0600

    added styling

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path =require("path");
+var path = require("path");
 
 var app = express();
 var port = process.env.PORT || 3000
@@ -13,8 +13,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("app/public"));
 
 
-require(".app/routing/api-routes.js")(app);
-require(".app/routing/html-routes.js")(app);
+require(".app/routing/apiRoutes")(app);
+require(".app/routing/htmlRoutes")(app);
 
 
 app.listen(port, () => console.log("Listening on port %s", port));
